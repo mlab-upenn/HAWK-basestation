2e1c17e14d9da49703ccbfbb9c774a0c58c2eb9d
Paul M. Gurniak <pgurniak@seas.upenn.edu>
2012-05-07 18:27:55 -0300

    Update netbench/src/kinect_calibration.h

--- netbench/src/kinect_calibration.h
+++ netbench/src/kinect_calibration.h
@@ -1,5 +1,16 @@
+/*
+Netbench: program for commmunication with the Intel Atom and Microsoft Kinect onboard the quadrotor.  This program receives data from the Intel Atom, decompresses it, and publishes to predefined topics in the ROS system.
 
-// Parameters taken from Nicolas Burrus's homepage:
+Author: Paul Gurniak (pgurniak@gmail.com)
+
+Date created: March 16, 2012
+
+Note that all files for this project can be found in our Git repository:
+https://github.com/mlab/HAWK-basestation
+
+*/
+
+// Parameters taken from Nicolas Burrus's homepage for calibration of the Microsoft Kinect
 
 // Color
 #define fx_rgb 5.2921508098293293e+02
